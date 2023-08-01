7263c8815c2ce7d623cee1c93a554519308d0df6
Mathieu Geukens <mr-zwets@protonmail.com>
2023-08-01 19:06:28 +0200

    added a switch to show fields for NFT metadata

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # BCMR Generator
 
-Fill-in form to create the BCMR Json file for a fungible CashTokens project.
+Fill-in form to create the BCMR Json file for a CashTokens project!
 
 This Json file can be hosted on the web on a domain name you control or can be pinned on IPFS with [nft.storage](https://nft.storage/).
--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+// see switchery docs
+let elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+elems.forEach(elem => {
+  const switchery = new Switchery(elem, { size: 'small', color:"#0ac18f"});
+});
+// Has NFTs switch
+let hasNFTs = false;
+const hasNFTsSwitch = document.querySelector('#hasNFTsSwitch');
+document.querySelector('#hasNFTsSwitch').checked = false;
+hasNFTsSwitch.onchange = () => showNFTfields();
+function showNFTfields() {
+  hasNFTs = !hasNFTs;
+  document.querySelector('#NFTfields').style.display = hasNFTs? "block": "none";
+}
+
+// Has NFTs switch
+let hasNFTimages = false;
+const hasImagesSwitch = document.querySelector('#hasImagesSwitch');
+document.querySelector('#hasImagesSwitch').checked = false;
+hasImagesSwitch.onchange = () => hasNFTimages = !hasNFTimages;
+
 const listUris = [ [] ];
 createList(listUris);
 
@@ -50,6 +71,11 @@ function createJsonObj() {
   const tokenName = document.getElementById("tokenName").value;
   const tokenDescription = document.getElementById("tokenDescription").value;
   const tokenSymbol = document.getElementById("tokenSymbol").value;
+  const numberNFTs = document.getElementById("numberNFTs").value;
+  const nftName = document.getElementById("nftName").value;
+  const nftDescription = document.getElementById("nftDescription").value;
+  const nftIconUri = document.getElementById("nftIconUri").value;
+  const nftIconType = document.getElementById("nftIconType").value;
   const webUrl = document.getElementById("webUrl").value;
   const tokenDecimals = document.getElementById("tokenDecimals").value;
 
@@ -59,7 +85,8 @@ function createJsonObj() {
   const registryIdentityName = `bcmr for ${tokenName}`;
   const registryIdentityDescription = `self-published bcmr for ${tokenName}`;
 
-  const hasRequiredFields = tokenId && tokenName && tokenDescription && tokenSymbol
+  let hasRequiredFields = tokenId && tokenName && tokenDescription && tokenSymbol;
+  if(hasNFTs) hasRequiredFields = hasRequiredFields && numberNFTs && nftName;
 
   if(!hasRequiredFields){
     alert("Fill in all the required fields before generating the JSON file!")
@@ -94,6 +121,27 @@ function createJsonObj() {
   }
   const snapshot = bcmrJsonObj.identities[tokenId][isoString];
   if(tokenDecimals) snapshot.token.decimals = parseInt(tokenDecimals);
+  if(hasNFTs){
+    snapshot.token.nfts = {
+      description: "",
+      parse: {
+        types: {}
+      }
+    };
+    const NFTtypes = snapshot.token.nfts.parse.types;
+    for(let i=1; i<=numberNFTs; i++){
+      let nftNameNumbered = nftName.replace("{i}", i);
+      let nftDescriptionNumbered = nftDescription.replace("{i}", i);
+      NFTtypes[i] = {
+        "name": nftNameNumbered,
+        "description": nftDescriptionNumbered ,
+        "uris": {
+          "icon": nftIconUri + `/${i}.${nftIconType}`
+        }
+      }
+      if(hasNFTimages) NFTtypes[i].uris.image = nftIconUri + `/${i}-img.${nftIconType}`
+    }
+  }
   if(webUrl) snapshot.uris.web = webUrl;
   listUris.forEach(uri => {
     if(uri[0] && uri[1]) snapshot.uris[uri[0]] = uri[1];
